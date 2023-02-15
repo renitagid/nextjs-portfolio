249326bb2b796027bf707103ae78c77848260885
renitagid <renita.y.ip@gmail.com>
2023-02-14 16:41:11 -0800

    light styling on about me, projects

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,24 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 import profile from "public/profile.jpg"
+import { motion } from "framer-motion";
 
 export default function AboutMe() {
   return (
     <div>
-      <section>
-        <Image src={profile} alt="" />
-      </section>
-      <section>
-        <h3>Technical Skills</h3>
+      <div>
+        <Image src={profile} alt="" className="rounded-full w-16 m-2"/>
+      </div>
+      <section className="border-2 border-black m-2 rounded-xl p-2 object-center">
+        <h3 className="text-xl">Technical Skills</h3>
         <div>List of skills and/or icons</div>
       </section>
-      <section>
-        Here is a summary of my background, skills, and general vibe
+      <section className="border-2 border-black m-2 rounded-xl p-2 object-center">
+      <h1>My story:</h1>
+       <p> Here is a summary of my background and general vibe.</p>
+       <h2>Transferrable Skills:</h2>
+       <p> Here is a summary of my transferrable skills</p>
       </section>
-      <h1>About Me</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
     </div>
   );
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
+import Image from "next/image"
+import project1Image from "public/project.png"
 
 const Projects = () => {
-  const [project, setProject] = useState("")
+  const [project, setProject] = useState("");
+  const [projectInfo, setProjectInfo] = useState("");
   const handleChange = (e) => {
-    setProject(e.target.value)
-  }
+    setProject(e.target.value);
+  };
+
   return (
-    
-    <section>
-      <h1>My projects</h1>
+    <section className="font-serif m-2">
+      <h1 className="text-xl">My projects</h1>
       <div>
         <label for="projects">Choose a project:</label>
 
         <select name="projects" id="projects" onChange={handleChange}>
-          <option value="project1">Project 1</option>
-          <option value="project2">Project 2</option>
+          <option></option>
+          <option value="project1">Capstone Project</option>
+          <option value="project2">Data Visualization Project</option>
           <option value="project3">Project 3</option>
           <option value="project4">Project 4</option>
         </select>
-        <div>
-          {project}
-        </div>
+        {project === "project1" ? (
+          <div>Capstone Project
+            <Image src={project1Image} alt="project 1 image" />
+          </div>
+        ) : project === "project2" ? (
+          <div>Data Visualization Project</div>
+        ) : project === "project3" ? (
+          <div>Project information about number 3</div>
+        ) :project === "project4" ? (
+          <div>Project information about number 4</div>
+        ) :null}
       </div>
     </section>
   );
