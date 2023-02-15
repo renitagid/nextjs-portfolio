541a5a7d3196e66a90e44d36310dac76f8425da9
renitagid <renita.y.ip@gmail.com>
2023-02-15 12:14:51 -0800

    resume styling

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,14 +4,14 @@ import Image from "next/image";
 
 const Resume = () => {
   return (
-    <>
-    <div className="border-2 border-black my-1 ml-1 text-center bg-gradient-to-br shadow-gray-700 shadow-md from-red-300 via-yellow-100 to-teal-200 w-40 rounded-lg font-serif">
+    <div className="border-2 border-black mt-12 mx-2 text-center text-sm bg-gradient-to-br shadow-gray-700 shadow-md from-red-300 via-yellow-100 to-teal-200 w-11/12 rounded-xl pb-3 font-serif">
+    <div>
     <a href="/renita-gidlund-resume.pdf" target="_blank">Download as PDF</a>
     </div>
     <div>
       <Image src={resume} alt="resume"/>
     </div>
-    </>
+    </div>
   );
 };
 
